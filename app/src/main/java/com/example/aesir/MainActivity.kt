2571048056beb1faf2d8aee5967954d28bc3b4aa
Riley Larche <riley.larche@gmail.com>
2019-09-20 20:59:33 -0700

    Bluetooth Device ID's Recognized and Displayed by App
    -Shows address in textbox

--- app/src/main/java/com/example/aesir/MainActivity.kt
+++ app/src/main/java/com/example/aesir/MainActivity.kt
@@ -3,17 +3,19 @@ package com.example.aesir
 import android.app.Activity
 import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothManager
+import android.bluetooth.le.ScanCallback
+import android.bluetooth.le.ScanResult
 import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.*
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     //Global Variables
     private val requestEnableBt: Int = 0
-    var mScanning: Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,25 +25,15 @@ class MainActivity : AppCompatActivity() {
         val searchButton = findViewById<Button>(R.id.search)
         val deviceList: ListView = findViewById<ExpandableListView>(R.id.device_list)
 
-        //Init Local Variables
-        val arraySize = 100
-        val listItems = arrayOfNulls<String>(arraySize)
-
-        //Run Setup Functions and Logic for MainActivity
-        val adapterBt: BluetoothAdapter? = setUpBluetooth() //Only checks when app is first enabled... problem?
-
         searchButton.setOnClickListener{
             //Re-Direct the search button to the following function:
             findBluetoothDevices()
         }
 
         //Old Array Code:
-        for(i in 0 until arraySize){
-            listItems[i] = "List Item: $i"
-        }
         //Create and set simple adapter for listView
-        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, listItems)
-        deviceList.adapter = adapter
+        //val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, listItems)
+        //deviceList.adapter = adapter
 
     }
 
@@ -67,9 +59,12 @@ class MainActivity : AppCompatActivity() {
     }
 
 
-    fun setUpBluetooth(): BluetoothAdapter? {
+    fun findBluetoothDevices(){
+        //Scan for devices (limited to kitkat or above, need older API for lower)
+        showToast("Checking that Bluetooth is Enabled.")
+
         //Get the Bluetooth Adapter
-        val bluetoothAdapter: BluetoothAdapter? by lazy(LazyThreadSafetyMode.NONE) {
+        val mBluetoothAdapter: BluetoothAdapter? by lazy(LazyThreadSafetyMode.NONE) {
             val bluetoothManager = getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager
             bluetoothManager.adapter
         }
@@ -77,20 +72,44 @@ class MainActivity : AppCompatActivity() {
         //Enable Bluetooth
         //If Bluetooth is available on the device but not enabled,
         //a dialog will open for the user to enable Bluetooth
-        bluetoothAdapter?.takeIf { !it.isEnabled }?.apply {
+        mBluetoothAdapter?.takeIf { !it.isEnabled }?.apply {
             val enableBtIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
             startActivityForResult(enableBtIntent, requestEnableBt)
         }
 
-        return bluetoothAdapter
+        //Get and instance of the Bluetooth Low Energy Scanner
+        val mScanner = mBluetoothAdapter?.bluetoothLeScanner
 
+        //Stop previous scan if any
+        mScanner?.stopScan(MCallBack())
+
+        //showToast(mBluetoothAdapter.address.toString())
+        textView.text = "Startting BT Scan...."
+        mScanner?.startScan(MCallBack())
+        //mScanner?.stopScan(MCallBack())
+        //mScanner?.flushPendingScanResults(MCallBack())
     }
 
 
-    fun findBluetoothDevices(){
-        //Scan for devices (limited to kitkat or above, need older API for lower)
-        showToast("Searching for Bluetooth LE Devices...")
+    inner class MCallBack: ScanCallback(){
+        override fun onScanResult(callbackType: Int, result: ScanResult?) {
+            super.onScanResult(callbackType, result)
+
+            textView.text = result?.device?.address
+        }
+
+        override fun onScanFailed(errorCode: Int) {
+            super.onScanFailed(errorCode)
+
+            textView.text = "Scan FAIL"
+        }
+
+        override fun onBatchScanResults(results: MutableList<ScanResult>?) {
+            super.onBatchScanResults(results)
+
+            textView.text = "Fuck me Scan"
+        }
     }
+
+
 }
-
-
