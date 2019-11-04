cb1a1b97fc4dee21f9742c1032634d466f75c78b
Riley Larche <36251088+programCounter@users.noreply.github.com>
2019-11-04 15:27:09 -0800

    Collapse sections of BSI setup
    When a sensor is not active, the sensor's config feilds are hidden and the UI is collapsed.

--- app/src/main/java/com/example/aesir/BSIFragment.kt
+++ app/src/main/java/com/example/aesir/BSIFragment.kt
@@ -1,7 +1,7 @@
 /*
 File Name: BSIFragment.kt
 Author: Riley Larche
-Date Updated: 2019-10-17
+Date Updated: 2019-11-04
 Android Studio Version:3.5.1
 Tested on Android Version: 10 and 8
 
@@ -25,6 +25,7 @@ import android.widget.ArrayAdapter
 import android.widget.Spinner
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.bsi_fragment.view.*
 
 
 //
@@ -35,11 +36,29 @@ class BSIFragment : Fragment(), AdapterView.OnItemSelectedListener {
     // Private VAL or VAR
     //
     private lateinit var mInterface: BSI
+    private var a1ui: List<TextView> = listOf()
+    private var a2ui: List<TextView> = listOf()
+    private var pui: List<TextView> = listOf()
+    private var a1spinner: Spinner? = null
+    private var a2spinner: Spinner? = null
+    private var pspinner: Spinner? = null
 
 
     //
     // Fragment Functions
     //
+    private fun showHide(elements: List<TextView>, show: Boolean) {
+        var vis = View.GONE
+        if (show) {
+            vis = View.VISIBLE
+        }
+
+        for (item in elements) {
+            //val uiElement = v.findViewById<TextView>(item)
+            item.visibility = vis
+        }
+    }
+
     // Runs when the fragment is being loaded by the FragmentManager in MainActivity
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -70,6 +89,10 @@ class BSIFragment : Fragment(), AdapterView.OnItemSelectedListener {
         }
 
         // Set adapter for spinner
+        a1spinner = view.findViewById(R.id.bsi_analog_1_spinner)
+        a2spinner = view.findViewById(R.id.bsi_analog_2_spinner)
+        pspinner = view.findViewById(R.id.bsi_pulse_spinner)
+
         val analog1spinner: Spinner = view.findViewById(R.id.bsi_analog_1_spinner)
         ArrayAdapter.createFromResource(context, R.array.bsi_spinner_options, android.R.layout.simple_spinner_item).also { adapter ->
             // Specify the layout to use when the list of choices appears
@@ -78,8 +101,48 @@ class BSIFragment : Fragment(), AdapterView.OnItemSelectedListener {
             analog1spinner.adapter = adapter
         }
 
+        val analog2spinner: Spinner = view.findViewById(R.id.bsi_analog_2_spinner)
+        ArrayAdapter.createFromResource(context, R.array.bsi_spinner_options, android.R.layout.simple_spinner_item).also { adapter ->
+            // Specify the layout to use when the list of choices appears
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            // Apply the adapter to the spinner
+            analog2spinner.adapter = adapter
+        }
+
+        val pulsespinner: Spinner = view.findViewById(R.id.bsi_pulse_spinner)
+        ArrayAdapter.createFromResource(context, R.array.bsi_spinner_options, android.R.layout.simple_spinner_item).also { adapter ->
+            // Specify the layout to use when the list of choices appears
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            // Apply the adapter to the spinner
+            pulsespinner.adapter = adapter
+        }
+
         // Interface instance is this because Fragment extends it
         analog1spinner.onItemSelectedListener = this
+        analog2spinner.onItemSelectedListener = this
+        pulsespinner.onItemSelectedListener = this
+
+        //Get UI elements for easy hiding or showing
+        a1ui = listOf(view.findViewById(R.id.bsi_power_on_delay_analog1),
+            view.findViewById(R.id.bsi_measurement_interval_analog1),
+            view.findViewById(R.id.bsi_power_on_delay_input_analog1),
+            view.findViewById(R.id.bsi_measurement_interval_input_analog1))
+        a2ui = listOf(view.findViewById(R.id.bsi_power_on_delay_analog2),
+            view.findViewById(R.id.bsi_measurement_interval_analog2),
+            view.findViewById(R.id.bsi_power_on_delay_input_analog2),
+            view.findViewById(R.id.bsi_measurement_interval_input_analog2))
+        pui = listOf(view.findViewById(R.id.bsi_power_on_delay_pulse),
+            view.findViewById(R.id.bsi_measurement_interval_pulse),
+            view.findViewById(R.id.bsi_power_on_delay_input_pulse),
+            view.findViewById(R.id.bsi_measurement_interval_input_pulse))
+
+        //Hide the UI elements by DEFAULT and select false in spinner
+        analog1spinner.setSelection(0)
+        analog2spinner.setSelection(0)
+        pulsespinner.setSelection(0)
+        showHide(a1ui, false)
+        showHide(a2ui, false)
+        showHide(pui, false)
     }
 
     // Runs when the view is attached (becomes one with) MainActivity
@@ -99,13 +162,44 @@ class BSIFragment : Fragment(), AdapterView.OnItemSelectedListener {
     // Spinner Listeners
     //
     override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
-        if (position == 1) {
-            // Do something when active
+        if (view != null) {
+            when (parent?.id) {
+                a1spinner?.id -> {
+                    if (position == 1) {
+                        //Sensor is active, show the UI elements
+                        showHide(a1ui, true)
+                    }
+                    else {
+                        showHide(a1ui, false)
+                    }
+                }
+
+                a2spinner?.id -> {
+                    if (position == 1) {
+                        //Sensor is active, show the UI elements
+                        showHide(a2ui, true)
+                    }
+                    else {
+                        showHide(a2ui, false)
+                    }
+                }
+
+                pspinner?.id -> {
+                    if (position == 1) {
+                        //Sensor is active, show the UI elements
+                        showHide(pui, true)
+                    }
+                    else {
+                        showHide(pui, false)
+                    }
+                }
+            }
+
         }
     }
 
     override fun onNothingSelected(parent: AdapterView<*>?) {
-        // Maybe do something here? Do I have to?
+        //Must be here for abstract error
     }
 
 
